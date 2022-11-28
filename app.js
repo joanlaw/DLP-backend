62aa4630323b77bc4087975e52debaa477383085
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2022-11-28 11:55:13 -0600

    prueba cors node

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const cors = require('cors')
 //const whitelist =  ['http://localhost:3000', 'https://duellinks.pro'];
 
 app.use(cors({
-  origin: 'http://127.0.0.1:3000',
+  origin: 'http://localhost:3000',
 }))
 app.use(morgan('dev'))
 //app.use(fileUpload({
