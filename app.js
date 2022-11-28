9fa08940f84f0148775393b82214cfc5299fd0eb
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2022-11-27 21:28:31 -0600

    prueba cors

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ const app = express()
 //const cors = require('cors')
 
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
 app.use(cors())
 app.use(morgan('dev'))
 //app.use(fileUpload({
