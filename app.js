7f2f2855704ddda5e0b0f42d85429989763daec6
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2022-11-28 11:53:22 -0600

    prueba cors node

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
-import cors from 'cors'
+//import cors from 'cors'
 
 
 import indexRoutes from './routes/index.routes.js'
@@ -9,7 +9,7 @@ import decksRoutes from './routes/decks.routes.js'
 //import fileUpload from 'express-fileupload'
 
 const app = express()
-//const cors = require('cors')
+const cors = require('cors')
 
 
 // Configurar cabeceras y cors
