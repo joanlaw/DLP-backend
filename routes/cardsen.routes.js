b6001656f1813b261a189d40fda6563d71f38cae
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-07-07 09:20:08 -0600

    dfjdk

--- routes/cardsen.routes.js
+++ routes/cardsen.routes.js
@@ -13,7 +13,9 @@ const cardsrouter = Router()
 import fileUpload from 'express-fileupload'
 
 cardsrouter.get('/cards', getCardsen)
-routerdecks.post('/cards', createCardsen)
+cardsrouter.post('/cards', fileUpload({
+  useTempFiles: true, tempFileDir: './uploads'
+}), createCardsen)
 cardsrouter.put('/cards/:id', updateCardsen)
 cardsrouter.delete('/cards/:id', deleteCardsen)
 cardsrouter.get('/cards/:id', getCarden)
