6c4576808f00245affd203de445899ae3e9c6b87
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2023-07-07 09:03:31 -0600

    permitir todos los origenes con cors

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors' // Importación correcta de cors
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
-import indexRoutes from './routes/index.routes.js'
-import cartasRoutes from './routes/cards.routes.js'
-import decksRoutes from './routes/decks.routes.js'
-import cardsRouter from './routes/cardsen.routes.js'
+import indexRoutes from './routes/index.routes.js';
+import cartasRoutes from './routes/cards.routes.js';
+import decksRoutes from './routes/decks.routes.js';
+import cardsRouter from './routes/cardsen.routes.js';
 
-const app = express()
+const app = express();
 
-const whitelist = ['http://localhost:3000', 'https://duellinks.pro']
+app.use(cors()); // Permitir todos los orígenes
 
-app.use(cors({
-  origin: whitelist
-}))
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
-app.use(indexRoutes)
-app.use(cartasRoutes)
-app.use(decksRoutes)
-app.use(cardsRouter)
+app.use(indexRoutes);
+app.use(cartasRoutes);
+app.use(decksRoutes);
+app.use(cardsRouter);
 
 app.use((req, res) => {
   res.status(404).send("Not Found");
 });
 
-export default app
+export default app;
